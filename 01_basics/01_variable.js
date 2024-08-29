8508b0168aea442b881e1c52229224a1b0f60e91
FatimaMuhammadYousuf <itsfatimamuhammadyousuf@gmail.com>
2024-08-29 12:07:00 +0000

    dataTypes

---
+++ 01_basics/01_variable.js
@@ -0,0 +1,21 @@
+ const account_id = 123
+ var account_pass = "1123"
+ let account_email= "fsy@gmail.com"
+  accountCity = "karachi"
+ let account_state;
+//  account_id = 143 // not allowed
+ 
+//  account_email = "fatima@gmail.com"
+//  account_pass = "21211"
+//  accountCity = "lahore"
+
+ console.log(account_email);
+ console.log(account_pass);
+ console.log(accountCity);
+ 
+ console.table([account_id,account_email,account_pass, accountCity,account_state])
+
+ /*
+ prefer not to use var coz of issue in block scope and functional scope
+  */
+ 
---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,40 @@
+"use strict"; // treat all code as newer version of js.
+
+//alert( 3 + 3) // using node js not browser
+// its can work with
+// html bind or directly console that generate popup of 6 
+//but in node to create alert there is different syntax
+
+// react and node avoid semicolon
+
+//console.log( 3 + 3 ); // code readability
+//console.log("fsy");
+
+let name = "fsy"
+let age = 18
+let islogIn = false
+let state;
+let my_gpa = null
+
+//no => 2 to power 53
+// biginit => no is to large
+// string  "" & '' // prefer ""
+// boolean => true/ false
+//null   => standalone value // represntation of empty value
+// undefined => no value assign 
+// sybmol => unique
+
+// object 
+
+console.log(typeof "fsy")
+console.log(typeof age)
+console.log(typeof islogIn)
+console.log(typeof state) // state is undefined type
+console.log(typeof my_gpa) // null is an object 
+
+// undefine means data types define ni h 
+
+
+
+
+
